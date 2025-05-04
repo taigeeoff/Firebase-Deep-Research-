7e447a65cb9d0a987d4b4f904a872b8053162c0d
poerschmann <poerschmann@google.com>
2025-05-04 10:16:51 +0000

    next & ts config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 import type { Configuration as WebpackConfig } from 'webpack';
 
 const nextConfig: NextConfig = {
+
+  // Add rewrite for .well-known path
+  async rewrites() {
+    return [
+      {
+        source: '/.well-known/:path*', // Match all paths under .well-known
+        destination: '/well-known/:path*', // Rewrite to the corresponding path under /well-known
+      },
+    ];
+  },
+
   // Enable webpack analyzer in production build
   webpack: (config: WebpackConfig, { isServer }: { isServer: boolean }): WebpackConfig => {
     // Add fallbacks for Node.js modules that are causing issues
